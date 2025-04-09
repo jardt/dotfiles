68ed56f492cb4f8b179393f9fa258fde75dd7289
Jardar Tøn <jardar.ton@bouvet.no>
2025-04-09 08:21:30 +0200

    test

--- nvim/.config/nvim/lua/plugins/folke.lua
+++ nvim/.config/nvim/lua/plugins/folke.lua
@@ -281,7 +281,7 @@ return {
 			indent = { enabled = true },
 			input = { enabled = true },
 			notifier = { enabled = true },
-			scope = { enabled = true },
+			scope = { enabled = false },
 			scroll = { enabled = false },
 			statuscolumn = { enabled = false }, -- we set this in options.lua
 			words = { enabled = true },
